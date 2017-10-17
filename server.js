7f395ec3ecfc963595191d6f15a7572354fb34ca
alexmorten <alexandrius.martinus@gmx.de>
2017-10-17 13:07:14 +0200

    basic working example

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ const io = socketIo(server);
 server.listen(port, () => console.log(`Listening on port ${port}`));
 let temp = null;
 app.post('/calendar',(req, res)=>{
-    let keys=Object.keys(req);
     console.log(req.body);
     res.send({});
     temp=req.body;
@@ -23,14 +22,27 @@ app.get('/',(req,res)=>{
     console.log("/");
     res.send(temp);
 });
+
+let messages = []; // <- temporary, shoudl be a db
+
 app.post('/slack',(req,res)=>{
     temp=req.body;
     console.log("-----SLACK-------");
-    console.log(req.body);
     res.send({});
-    io.emit("slack_message",{text:req.body.text});
+    let message = req.body;
+    console.log(message);
+    let slimMessage = {
+        text:message.text,
+        user:{
+            name:message.user.name,
+            profile:message.user.profile,
+        }
+    };    
+    messages.push(slimMessage);
+    io.emit("slack_message",slimMessage);
 }); 
 io.on("connection",(socket)=>{
+    socket.emit("all_messages",messages); // <- REALLY temporary
     console.log("client connected");
     socket.on("disconnect",()=>{
         console.log("client disconnected");
