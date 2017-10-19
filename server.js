f7c75b3a8df37aee38ced9339fd061b600549937
jonasbeisswenger <jonas.beisswenger@code.berlin>
2017-10-19 14:37:14 +0200

    Added Calendar & Slack API Last Stand from Samuel

--- server.js
+++ server.js
@@ -8,22 +8,17 @@ var MongoClient = require('mongodb').MongoClient;
 var mongoose = require('mongoose');
 app.use(cors());
 app.use(bodyParser.json());
+const server = http.createServer(app);
+const io = socketIo(server);
 
 //Connect to Mongoose (MongoDB)
 mongoose.connect('mongodb://admin:testmongodb@codeboard-shard-00-00-iqwvb.mongodb.net:27017,codeboard-shard-00-01-iqwvb.mongodb.net:27017,codeboard-shard-00-02-iqwvb.mongodb.net:27017/test?ssl=true&replicaSet=CodeBoard-shard-0&authSource=admin');
 var db = mongoose.connection;
 
-const server = http.createServer(app);
-
-const io = socketIo(server);
-
 server.listen(port, () => console.log(`Listening on port ${port}`));
 let temp = null;
-app.post('/calendar',(req, res)=>{
-    console.log(req.body);
-    res.send({});
-    temp=req.body;
-  });
+
+//Test
 app.get('/',(req,res)=>{
     console.log("/");
     res.send('Hello World');
@@ -32,6 +27,7 @@ app.get('/',(req,res)=>{
 
 let messages = []; // <- temporary, shoudl be a db
 
+//Slack Zapier API REST
 app.post('/slack',(req,res)=>{
     temp=req.body;
     console.log("-----SLACK-------");
@@ -49,6 +45,40 @@ app.post('/slack',(req,res)=>{
     io.emit("slack_message",slimMessage);
 });
 
+//Calendar API Zapier REST
+app.post('/calendar',(req, res)=>{
+    console.log(req.body);
+    res.send({});
+    temp=req.body;
+    console.log("-----CALENDAR-------");
+    res.send({});
+    let message = req.body;
+    console.log(message);
+    let slimMessage = {
+          end:{
+                dateTime:message.end.dateTime,
+              },
+          description:message.description,
+          summary:message.summary,
+          start:
+          {
+            time_pretty:message.start.time_pretty,
+            dateTime:message.start.dateTime,
+          },
+          duration_minutes:message.duration_minutes,
+          location:message.location,
+          id:message.id,
+
+    };
+
+    console.log("-------THIS IS WHERE THE CALENDAR SLIMMESSAGE IS SUPPOSED TO START-------");
+    console.log(slimMessage);
+    messages.push(slimMessage);
+    io.emit("slack_message",slimMessage);
+
+  });
+
+//Send data to Socket.io
 io.on("connection",(socket)=>{
     socket.emit("all_messages",messages); // <- REALLY temporary
     console.log("client connected");
